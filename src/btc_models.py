f0c30830541c43a49b9dca430f50fd579643f18f
Qixun Qu <quqixun@gmail.com>
2017-10-20 21:25:02 +0200

    Update name scope and comments for basic helper functions

--- src/btc_models.py
+++ src/btc_models.py
@@ -16,10 +16,21 @@
 #    '$$$$'.^.'$$$$'
 #       '&$$$$$&'
 
+'''
+
+Class BTCModels
+
+-1- Define several basic helper functions, which are the
+    basic modules to build simple CNN models.
+-2- Define several specific helper functions to construct
+    CNN models with more complicate structures.
+-3- Build models: CNN, Full-CNN, Res-CNN and Dense-CNN.
+
+'''
+
 
 import tensorflow as tf
 from operator import mul
-from btc_settings import *
 from functools import reduce
 from tensorflow.contrib.layers import xavier_initializer
 
@@ -27,83 +38,172 @@ from tensorflow.contrib.layers import xavier_initializer
 class BTCModels():
 
     def __init__(self, net, classes, act="relu", alpha=None,
-                 momentum=0.99, train_bn=True, bc=True):
+                 momentum=0.99, drop_rate=0.5):
         '''__INIT__
+
+            Initialization of BTCModels. In this functions,
+            commen parameters of all models are set first.
+
+            Inputs:
+            -------
+            - net: string, the name of the model applied to train
+            - classes: int, the number of grading groups
+            - act: string, indicate the activation method by either
+                   "relu" or "lrelu" (leaky relu)
+            - alpha: float, slope of the leaky relu at x < 0
+            - momentum: float, momentum for removing average in
+                        batch normalization, typically values are
+                        0.999, 0.99, 0.9, etc
+            - drop_rate: float, rate of dropout of input units,
+                         which is between 0 and 1
+
         '''
+
+        self.classes = classes
 
         self.act = act
         self.alpha = alpha
         self.momentum = momentum
-        self.train_bn = train_bn
-        self.classes = classes
+        self.drop_rate = drop_rate
 
-        if net == DENSE_CNN:
-            self.bc = bc
+        # A symbol to indicate whether the model is used to train
+        # The symbol will be assigned as a placeholder while
+        # feeding the model in training and validating steps
+        self.training = None
 
         return
 
     #
-    # Helper Functions
+    # Basic Helper Functions
     #
 
     def _conv3d(self, x, filters, kernel_size, strides=1,
                 padding="same", name="conv_var"):
         '''_CONV3D
 
-            Full:  self._conv3d(x, 32, 3, 1, "same", "conv")
-            Short: self._conv3d(x, 32, 3)
+            Return 3D convolution layer with variables
+            initialized by xavier method.
+
+            Usages:
+            -------
+            - full:  self._conv3d(x, 32, 3, 1, "same", "conv")
+            - short: self._conv3d(x, 32, 3)
+
+            Inputs:
+            -------
+            - x: tensor, input layer
+            - filters: int, the number of kernels
+            - kernel_size: int, the size of cube kernel
+            - strides: int, strides along three dimentions
+            - padding: string, "same" or "valid"
+            - name: string, layer's name
+
+            Output:
+            -------
+            - a 3D convolution layer
 
         '''
 
-        return tf.layers.conv3d(inputs=x,
-                                filters=filters,
-                                kernel_size=kernel_size,
-                                strides=strides,
-                                padding=padding,
-                                kernel_initializer=xavier_initializer(),
-                                name=name)
+        with tf.name_scope("conv3d"):
+            return tf.layers.conv3d(inputs=x,
+                                    filters=filters,
+                                    kernel_size=kernel_size,
+                                    strides=strides,
+                                    padding=padding,
+                                    kernel_initializer=xavier_initializer(),
+                                    name=name)
 
     def _fully_connected(self, x, units, name="fc_var"):
-        '''_FULLy_CONNECTED
+        '''_FULLY_CONNECTED
 
-            Full:  self._fully_connected(x, 128, "fc")
-            short: self._fully_connected(x, 128)
+            Return fully connected layer with variables
+            initialized by xavier method.
+
+            Usages:
+            -------
+            - full:  self._fully_connected(x, 128, "fc")
+            - short: self._fully_connected(x, 128)
+
+            Inputs:
+            -------
+            - x: tensor, input layer
+            - units: int, the number of neurons
+            - name: string, layer's name
+
+            Outputs:
+            --------
+            - a fully connected layer
 
         '''
 
-        return tf.layers.dense(inputs=x,
-                               units=units,
-                               kernel_initializer=xavier_initializer(),
-                               name=name)
+        with tf.name_scope("full_connection"):
+            return tf.layers.dense(inputs=x,
+                                   units=units,
+                                   kernel_initializer=xavier_initializer(),
+                                   name=name)
 
     def _batch_norm(self, x, name="bn_var"):
         '''_BATCH_NORM
 
-            Full:  self._batch_norm(x, "bn")
-            Short: self._batch_norm(x)
+            Normalize the input layer.
+            Momentum and symbol of training have been
+            assigned while the class is initialized.
+
+            Usages:
+            -------
+            - full:  self._batch_norm(x, "bn")
+            - short: self._batch_norm(x)
+
+            Inputs:
+            -------
+            - x: tensor, input layer
+            - name: string, layer's name
+
+            Output:
+            -------
+            - normalized layer
 
         '''
 
-        return tf.contrib.layers.batch_norm(inputs=x,
-                                            decay=self.momentum,
-                                            is_training=self.train_bn,
-                                            scope=name)
+        with tf.name_scope("batch_norm"):
+            return tf.contrib.layers.batch_norm(inputs=x,
+                                                decay=self.momentum,
+                                                is_training=self.training,
+                                                scope=name)
 
     def _activate(self, x, name="act"):
         '''_ACTIVATE
 
-           Full:  self._activate(x, "act")
-           Short: self._activate(x)
+           Activate input layer. Two approaches can be available,
+           which are ReLU or leaky ReLU.
+           Activation method and setting has been set while the
+           class is initialized.
+
+           Usages:
+           -------
+           - full:  self._activate(x, "act")
+           - short: self._activate(x)
+
+           Inputs:
+           -------
+           - x: tensor, input layer
+           - name: string, layer's name
+
+           Output:
+           -------
+           - an activated layer
 
         '''
 
-        if self.act == "relu":
-            return tf.nn.relu(x, name)
-        elif self.act == "lrelu":
-            alpha = 0.2 if self.alpha is None else self.alpha
-            return tf.nn.leaky_relu(x, alpha, name)
-        else:
-            raise ValueError("Could not find act in ['relu', 'lrelu']")
+        with tf.name_scope("activate"):
+            if self.act == "relu":
+                return tf.nn.relu(x, name)
+            elif self.act == "lrelu":
+                # Set slope of leaky ReLU
+                alpha = 0.2 if self.alpha is None else self.alpha
+                return tf.nn.leaky_relu(x, alpha, name)
+            else:  # Raise error if activation method cannot be found
+                raise ValueError("Could not find act in ['relu', 'lrelu']")
 
         return
 
@@ -111,21 +211,39 @@ class BTCModels():
                        name="cba", padding="same", act=True):
         '''_CONV3D_BN_ACT
 
-            Full:  self._conv3d_bn_act(x, 32, 3, 1, "cba", same", True)
-            Short: self._conv3d_bn_act(x, 32, 3, 1, "cba")
+            A convolution block, including three sections:
+            - 3D convolution layer
+            - batch normalization
+            - activation
+
+            Usages:
+            -------
+            - full:  self._conv3d_bn_act(x, 32, 3, 1, "cba", same", True)
+            - short: self._conv3d_bn_act(x, 32, 3, 1, "cba")
+
+            Inputs:
+            -------
+            - x: tensor, input layer
+            - filters: int, the number of kernels
+            - kernel_size: int, the size of cube kernel
+            - strides: int, strides along three dimentions
+            - name: string, layer's name
+            - padding: string, "same" or "valid"
+            - act: string or None, indicates the activation,
+                   method, id None, return inactivated layer
+
+            Output:
+            - a convoluted, normalized and activated (if not None) layer
 
         '''
 
         with tf.variable_scope(name):
-            with tf.name_scope("conv3d"):
-                cba = self._conv3d(x, filters, kernel_size, strides, padding)
-            with tf.name_scope("batch_norm"):
-                cba = self._batch_norm(cba)
-            if act:  # check for residual block
-                with tf.name_scope("activate"):
-                    cba = self._activate(cba)
+            cba = self._conv3d(x, filters, kernel_size, strides, padding)
+            cba = self._batch_norm(cba)
+            if act:  # If act is None, return inactivated result
+                cba = self._activate(cba)
 
-            return cba
+        return cba
 
     def _fc_bn_act(self, x, units, name="fba"):
         '''_FULLY_CONNECTED
@@ -136,16 +254,13 @@ class BTCModels():
         '''
 
         with tf.variable_scope(name):
-            with tf.name_scope("full_connection"):
-                fba = self._fully_connected(x, units)
-            with tf.name_scope("batch_norm"):
-                fba = self._batch_norm(fba)
-            with tf.name_scope("activate"):
-                fba = self._activate(fba)
+            fba = self._fully_connected(x, units)
+            fba = self._batch_norm(fba)
+            fba = self._activate(fba)
 
-            return fba
+        return fba
 
-    def _max_pool(self, x, psize=2, stride=2, name="max_pool"):
+    def _max_pool(self, x, psize=2, name="max_pool"):
         '''_MAX_POOL
 
             Full:  self._max_pool(x, 2, 2, "max_pool")
@@ -155,10 +270,10 @@ class BTCModels():
 
         return tf.layers.max_pooling3d(inputs=x,
                                        pool_size=psize,
-                                       strides=stride,
+                                       strides=psize,
                                        name=name)
 
-    def _average_pool(self, x, psize=2, stride=2, name="avg_pool"):
+    def _average_pool(self, x, psize=2, name="avg_pool"):
         '''_AVERAGE_POOL
 
             Full:  self._average_pool(x, 2, 2, "avg_pool")
@@ -168,10 +283,10 @@ class BTCModels():
 
         return tf.layers.average_pooling3d(inputs=x,
                                            pool_size=psize,
-                                           strides=stride,
+                                           strides=psize,
                                            name=name)
 
-    def _flatten(self, x, name="flatten"):
+    def _flatten(self, x, name="flt"):
         '''_FLATTEN
 
             Full:  self._flatten(x, "flatten")
@@ -182,7 +297,8 @@ class BTCModels():
         x_shape = x.get_shape().as_list()
         f_shape = reduce(mul, x_shape[1:], 1)
 
-        return tf.reshape(tensor=x, shape=[-1, f_shape], name=name)
+        with tf.name_scope("flatten"):
+            return tf.reshape(tensor=x, shape=[-1, f_shape], name=name)
 
     def _dropout(self, x, name="dropout"):
         '''_DROP_OUT
@@ -191,11 +307,8 @@ class BTCModels():
 
         '''
 
-        return tf.layers.dropout(inputs=x, rate=self.drop_rate, name=name)
-
-    #
-    # Helper function for cnn
-    #
+        return tf.layers.dropout(inputs=x, rate=self.drop_rate,
+                                 training=self.training, name=name)
 
     def _logits_fc(self, x, name="logits"):
         '''_LOGITS_FC
@@ -205,7 +318,8 @@ class BTCModels():
 
         '''
 
-        return self._fully_connected(x, self.classes, name)
+        with tf.variable_scope(name):
+            return self._fully_connected(x, self.classes, name)
 
     #
     # Helper function for full cnn
@@ -222,8 +336,7 @@ class BTCModels():
         x_shape = x.get_shape().as_list()
 
         with tf.variable_scope(name):
-            with tf.name_scope("conv3d"):
-                return self._conv3d(x, self.classes, x_shape[1:-1], 1, "valid")
+            return self._conv3d(x, self.classes, x_shape[1:-1], 1, "valid")
 
     #
     # Helper function for residual cnn
@@ -250,8 +363,8 @@ class BTCModels():
 
         with tf.name_scope(name + "_add"):
             res = tf.add(res, x)
-        with tf.name_scope(name + "_activate"):
-            return self._activate(res)
+
+        return self._activate(res)
 
     #
     # Helpher functions for dense cnn
@@ -261,14 +374,10 @@ class BTCModels():
         '''_COPOSITE
         '''
 
-        with tf.name_scope("batch_norm"):
-            comp = self._batch_norm(x)
-        with tf.name_scope("activate"):
-            comp = self._activate(comp)
-        with tf.name_scope("conv3d"):
-            comp = self._conv3d(comp, filters, kernel_size)
-        with tf.name_scope("dropout"):
-            comp = self._dropout(comp)
+        comp = self._batch_norm(x)
+        comp = self._activate(comp)
+        comp = self._conv3d(comp, filters, kernel_size)
+        comp = self._dropout(comp)
 
         return comp
 
@@ -276,14 +385,10 @@ class BTCModels():
         '''_BOTTLENECK
         '''
 
-        with tf.name_scope("batch_norm"):
-            bott = self._batch_norm(x)
-        with tf.name_scope("activate"):
-            bott = self._activate(bott)
-        with tf.name_scope("conv3d"):
-            bott = self._conv3d(bott, filters * 4, 1, padding="valid")
-        with tf.name_scope("dropout"):
-            bott = self._dropout(bott)
+        bott = self._batch_norm(x)
+        bott = self._activate(bott)
+        bott = self._conv3d(bott, filters * 4, 1, padding="valid")
+        bott = self._dropout(bott)
 
         return bott
 
@@ -319,8 +424,7 @@ class BTCModels():
         out_channels = x.get_shape().as_list()[-1]
         with tf.variable_scope(name + "_comp"):
             tran = self._composite(x, out_channels, 1)
-        with tf.name_scope(name + "_avgpool"):
-            tran = self._average_pool(tran)
+        tran = self._average_pool(tran, 2, name + "_avgpool")
 
         return tran
 
@@ -329,14 +433,11 @@ class BTCModels():
         '''
 
         with tf.variable_scope("last_trans"):
-            with tf.name_scope("batch_norm"):
-                last_tran = self._batch_norm(x)
-            with tf.name_scope("activate"):
-                last_tran = self._activate(last_tran)
+            last_tran = self._batch_norm(x)
+            last_tran = self._activate(last_tran)
 
         out_channels = last_tran.get_shape().as_list()[-1]
-        with tf.name_scope("global_avgpool"):
-            last_tran = self._average_pool(last_tran, out_channels, out_channels)
+        last_tran = self._average_pool(last_tran, out_channels, "global_avgpool")
 
         return last_tran
 
@@ -348,7 +449,7 @@ class BTCModels():
         '''_TEST
         '''
 
-        self.drop_rate = 0.5
+        self.training = True
 
         x = tf.placeholder(tf.float32, [5, 36, 36, 36, 4], "input")
         cba1 = self._conv3d_bn_act(x, 2, 3, 1, "layer1")
@@ -375,68 +476,69 @@ class BTCModels():
     # Contruct Models
     #
 
-    def cnn(self, x, drop_rate):
+    def cnn(self, x, training):
         '''CNN
 
         '''
 
-        self.drop_rate = drop_rate
+        self.training = training
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d_bn_act(x, 1, 3, 1, "layer1")
-        net = self._max_pool(net, 2, 2, "max_pool1")
+        net = self._max_pool(net, 2, "max_pool1")
         net = self._conv3d_bn_act(net, 1, 3, 1, "layer2")
-        net = self._max_pool(net, 2, 2, "max_pool2")
+        net = self._max_pool(net, 2, "max_pool2")
         net = self._conv3d_bn_act(net, 1, 3, 1, "layer3")
-        net = self._max_pool(net, 2, 2, "max_pool3")
+        net = self._max_pool(net, 2, "max_pool3")
         net = self._flatten(net, "flatten")
         net = self._fc_bn_act(net, 3, "fc1")
-        net = self._dropout(net, "drop1")
+        net = self._dropout(net, "dropout1")
         net = self._fc_bn_act(net, 3, "fc2")
-        net = self._dropout(net, "drop2")
+        net = self._dropout(net, "dropout2")
         net = self._logits_fc(net, "logits")
 
         return net
 
-    def full_cnn(self, x, drop_rate):
+    def full_cnn(self, x, training):
         '''FULL_CNN
         '''
 
-        self.drop_rate = drop_rate
+        self.training = training
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d_bn_act(x, 1, 3, 1, "layer1")
-        net = self._max_pool(net, 2, 2, "max_pool1")
+        net = self._max_pool(net, 2, "max_pool1")
         net = self._conv3d_bn_act(net, 1, 3, 1, "layer2")
-        net = self._max_pool(net, 2, 2, "max_pool2")
+        net = self._max_pool(net, 2, "max_pool2")
         net = self._conv3d_bn_act(net, 1, 3, 1, "layer3")
-        net = self._max_pool(net, 2, 2, "max_pool3")
+        net = self._max_pool(net, 2, "max_pool3")
         net = self._logits_conv(net, "logits_conv")
         net = self._flatten(net, "logits_flatten")
 
         return net
 
-    def res_cnn(self, x, drop_rate):
+    def res_cnn(self, x, training):
         '''RES_CNN
         '''
 
-        self.drop_rate = drop_rate
+        self.training = training
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d_bn_act(x, 1, 5, 2, "preconv")
         net = self._res_block(net, [1, 1, 1], 2, "res1")
         net = self._res_block(net, [1, 1, 2], 2, "res2")
-        net = self._max_pool(net, 7, 7, "max_pool")
+        net = self._max_pool(net, 7, "max_pool")
         net = self._flatten(net, "flatten")
         net = self._logits_fc(net, "logits")
 
         return net
 
-    def dense_cnn(self, x, drop_rate):
+    def dense_cnn(self, x, training):
         '''DENSE_NET
         '''
 
-        self.drop_rate = drop_rate
+        self.training = training
+        self.bc = True
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d(x, 1, 5, 2, "same", "preconv")
@@ -452,6 +554,6 @@ class BTCModels():
 
 if __name__ == "__main__":
 
-    models = BTCModels("dense_cnn", classes=3, act="relu", alpha=None,
-                       momentum=0.99, train_bn=True)
+    models = BTCModels("test", classes=3, act="relu", alpha=None,
+                       momentum=0.99, drop_rate=0.5)
     models._test()
