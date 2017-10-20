2482007fcaea999f9eed9041c4b308493916193a
Qixun Qu <quqixun@gmail.com>
2017-10-20 02:49:15 +0200

    Complete helper function and a simple test of dense net

--- src/btc_models.py
+++ src/btc_models.py
@@ -2,7 +2,7 @@
 # Script for Creating Models
 # Author: Qixun Qu
 # Create on: 2017/10/12
-# Modify on: 2017/10/18
+# Modify on: 2017/10/20
 
 #     ,,,         ,,,
 #   ;"   ';     ;'   ",
@@ -26,7 +26,7 @@ from tensorflow.contrib.layers import xavier_initializer
 
 class BTCModels():
 
-    def __init__(self, net, act="relu", alpha=None,
+    def __init__(self, net, classes, act="relu", alpha=None,
                  momentum=0.99, train_bn=True, bc=True):
         '''__INIT__
         '''
@@ -35,6 +35,7 @@ class BTCModels():
         self.alpha = alpha
         self.momentum = momentum
         self.train_bn = train_bn
+        self.classes = classes
 
         if net == DENSE_CNN:
             self.bc = bc
@@ -83,10 +84,10 @@ class BTCModels():
 
         '''
 
-        return tf.layers.batch_normalization(inputs=x,
-                                             momentum=self.momentum,
-                                             training=self.train_bn,
-                                             name=name)
+        return tf.contrib.layers.batch_norm(inputs=x,
+                                            decay=self.momentum,
+                                            is_training=self.train_bn,
+                                            scope=name)
 
     def _activate(self, x, name="act"):
         '''_ACTIVATE
@@ -155,7 +156,6 @@ class BTCModels():
         return tf.layers.max_pooling3d(inputs=x,
                                        pool_size=psize,
                                        strides=stride,
-                                       padding="valid",
                                        name=name)
 
     def _average_pool(self, x, psize=2, stride=2, name="avg_pool"):
@@ -169,7 +169,6 @@ class BTCModels():
         return tf.layers.average_pooling3d(inputs=x,
                                            pool_size=psize,
                                            strides=stride,
-                                           padding="valid",
                                            name=name)
 
     def _flatten(self, x, name="flatten"):
@@ -185,40 +184,38 @@ class BTCModels():
 
         return tf.reshape(tensor=x, shape=[-1, f_shape], name=name)
 
-    def _dropout(self, x, name="dropout", drop_rate=0.5):
+    def _dropout(self, x, name="dropout"):
         '''_DROP_OUT
 
-            Full:  self._dropout(x, "dropout", 0.5)
-                   self._dropout(x, "dropout", 0)
-            Short: self._dropout(x, "dropout")  # for training
+            Full:  self._dropout(x, "dropout")
 
         '''
 
-        return tf.layers.dropout(inputs=x, rate=drop_rate, name=name)
+        return tf.layers.dropout(inputs=x, rate=self.drop_rate, name=name)
 
     #
     # Helper function for cnn
     #
 
-    def _logits_fc(self, x, classes, name="logits"):
+    def _logits_fc(self, x, name="logits"):
         '''_LOGITS_FC
 
-            Full:  self._logits_fc(x, 3, "logits")
-            Short: self._logits_fc(x, 3)
+            Full:  self._logits_fc(x, "logits")
+            Short: self._logits_fc(x)
 
         '''
 
-        return self._fully_connected(x, classes, name)
+        return self._fully_connected(x, self.classes, name)
 
     #
     # Helper function for full cnn
     #
 
-    def _logits_conv(self, x, classes, name="logits"):
+    def _logits_conv(self, x, name="logits"):
         '''_LOGITS_CONV
 
-            Full:  self._logits_conv(x, 3, "logits")
-            Short: self._logits_conv(x, 3)
+            Full:  self._logits_conv(x, "logits")
+            Short: self._logits_conv(x)
 
         '''
 
@@ -226,7 +223,7 @@ class BTCModels():
 
         with tf.variable_scope(name):
             with tf.name_scope("conv3d"):
-                return self._conv3d(x, classes, x_shape[1:-1], 1, "valid")
+                return self._conv3d(x, self.classes, x_shape[1:-1], 1, "valid")
 
     #
     # Helper function for residual cnn
@@ -257,12 +254,101 @@ class BTCModels():
             return self._activate(res)
 
     #
+    # Helpher functions for dense cnn
+    #
+
+    def _composite(self, x, filters, kernel_size=3, name="composite"):
+        '''_COPOSITE
+        '''
+
+        with tf.name_scope("batch_norm"):
+            comp = self._batch_norm(x)
+        with tf.name_scope("activate"):
+            comp = self._activate(comp)
+        with tf.name_scope("conv3d"):
+            comp = self._conv3d(comp, filters, kernel_size)
+        with tf.name_scope("dropout"):
+            comp = self._dropout(comp)
+
+        return comp
+
+    def _bottleneck(self, x, filters, name="bottleneck"):
+        '''_BOTTLENECK
+        '''
+
+        with tf.name_scope("batch_norm"):
+            bott = self._batch_norm(x)
+        with tf.name_scope("activate"):
+            bott = self._activate(bott)
+        with tf.name_scope("conv3d"):
+            bott = self._conv3d(bott, filters * 4, 1, padding="valid")
+        with tf.name_scope("dropout"):
+            bott = self._dropout(bott)
+
+        return bott
+
+    def _dense_internal(self, x, growth_rate, no, name):
+        '''_DENSE_INTERNAL
+        '''
+
+        dint = x
+        if self.bc:
+            with tf.variable_scope(name + "_bott" + no):
+                dint = self._bottleneck(x, growth_rate)
+        with tf.variable_scope(name + "_comp" + no):
+            dint = self._composite(dint, growth_rate, 3)
+        with tf.name_scope(name + "_concat" + no):
+            dint = tf.concat((x, dint), 4)
+
+        return dint
+
+    def _dense_block(self, x, growth_rate, internals, name="dense_block"):
+        '''_DENSE_BLOCK
+        '''
+
+        dense = x
+        for internal in range(internals):
+            dense = self._dense_internal(dense, growth_rate, str(internal + 1), name)
+
+        return dense
+
+    def _transition(self, x, name="transition"):
+        '''_TRANSITION
+        '''
+
+        out_channels = x.get_shape().as_list()[-1]
+        with tf.variable_scope(name + "_comp"):
+            tran = self._composite(x, out_channels, 1)
+        with tf.name_scope(name + "_avgpool"):
+            tran = self._average_pool(tran)
+
+        return tran
+
+    def _last_transition(self, x):
+        '''_LOGITS_DENSE
+        '''
+
+        with tf.variable_scope("last_trans"):
+            with tf.name_scope("batch_norm"):
+                last_tran = self._batch_norm(x)
+            with tf.name_scope("activate"):
+                last_tran = self._activate(last_tran)
+
+        out_channels = last_tran.get_shape().as_list()[-1]
+        with tf.name_scope("global_avgpool"):
+            last_tran = self._average_pool(last_tran, out_channels, out_channels)
+
+        return last_tran
+
+    #
     # A Simple Test Case
     #
 
     def _test(self):
         '''_TEST
         '''
+
+        self.drop_rate = 0.5
 
         x = tf.placeholder(tf.float32, [5, 36, 36, 36, 4], "input")
         cba1 = self._conv3d_bn_act(x, 2, 3, 1, "layer1")
@@ -273,9 +359,9 @@ class BTCModels():
         max3 = self._max_pool(cba3, 2, 2, "max_pool3")
         flat = self._flatten(max3, "flatten")
         fcn1 = self._fc_bn_act(flat, 64, "fcn1")
-        drp1 = self._dropout(fcn1, "drop1", 0.5)
+        drp1 = self._dropout(fcn1, "drop1")
         fcn2 = self._fc_bn_act(drp1, 64, "fcn2")
-        drp2 = self._dropout(fcn2, "drop2", 0)
+        drp2 = self._dropout(fcn2, "drop2")
         outp = self._logits_fc(drp2, 3, "logits")
         prob = tf.nn.softmax(logits=outp, name="softmax")
 
@@ -289,10 +375,12 @@ class BTCModels():
     # Contruct Models
     #
 
-    def cnn(self, x, classes, drop_rate=0.5):
+    def cnn(self, x, drop_rate):
         '''CNN
 
         '''
+
+        self.drop_rate = drop_rate
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d_bn_act(x, 1, 3, 1, "layer1")
@@ -303,16 +391,18 @@ class BTCModels():
         net = self._max_pool(net, 2, 2, "max_pool3")
         net = self._flatten(net, "flatten")
         net = self._fc_bn_act(net, 3, "fc1")
-        net = self._dropout(net, "drop1", drop_rate)
+        net = self._dropout(net, "drop1")
         net = self._fc_bn_act(net, 3, "fc2")
-        net = self._dropout(net, "drop2", drop_rate)
-        net = self._logits_fc(net, classes, "logits")
+        net = self._dropout(net, "drop2")
+        net = self._logits_fc(net, "logits")
 
         return net
 
-    def full_cnn(self, x, classes, drop_rate=0.5):
+    def full_cnn(self, x, drop_rate):
         '''FULL_CNN
         '''
+
+        self.drop_rate = drop_rate
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d_bn_act(x, 1, 3, 1, "layer1")
@@ -321,14 +411,16 @@ class BTCModels():
         net = self._max_pool(net, 2, 2, "max_pool2")
         net = self._conv3d_bn_act(net, 1, 3, 1, "layer3")
         net = self._max_pool(net, 2, 2, "max_pool3")
-        net = self._logits_conv(net, classes, "logits_conv")
+        net = self._logits_conv(net, "logits_conv")
         net = self._flatten(net, "logits_flatten")
 
         return net
 
-    def res_cnn(self, x, classes, drop_rate=0.5):
+    def res_cnn(self, x, drop_rate):
         '''RES_CNN
         '''
+
+        self.drop_rate = drop_rate
 
         # Here is a very simple case to test btc_train first
         net = self._conv3d_bn_act(x, 1, 5, 2, "preconv")
@@ -336,21 +428,30 @@ class BTCModels():
         net = self._res_block(net, [1, 1, 2], 2, "res2")
         net = self._max_pool(net, 7, 7, "max_pool")
         net = self._flatten(net, "flatten")
-        net = self._logits_fc(net, classes, "logits")
+        net = self._logits_fc(net, "logits")
 
         return net
 
-    def dense_cnn(self, x, classes, drop_rate=0.5):
+    def dense_cnn(self, x, drop_rate):
         '''DENSE_NET
         '''
 
-        # To be finished
+        self.drop_rate = drop_rate
 
-        return
+        # Here is a very simple case to test btc_train first
+        net = self._conv3d(x, 1, 5, 2, "same", "preconv")
+        net = self._dense_block(net, 1, 2, "dense1")
+        net = self._transition(net, "trans1")
+        net = self._dense_block(net, 1, 2, "dense2")
+        net = self._last_transition(net)
+        net = self._flatten(net, "flatten")
+        net = self._logits_fc(net, "logits")
+
+        return net
 
 
 if __name__ == "__main__":
 
-    models = BTCModels("test", act="relu", alpha=None,
+    models = BTCModels("dense_cnn", classes=3, act="relu", alpha=None,
                        momentum=0.99, train_bn=True)
     models._test()
