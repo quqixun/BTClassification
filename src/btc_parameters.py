ccad4deae6144bd353aea0ea090cb51613b9a97d
Qixun Qu <quqixun@gmail.com>
2017-10-28 21:52:36 +0200

    Add parameters for autoencoder

--- src/btc_parameters.py
+++ src/btc_parameters.py
@@ -2,7 +2,7 @@
 # Script for Hyper-Parameters
 # Author: Qixun Qu
 # Create on: 2017/10/14
-# Modify on: 2017/10/26
+# Modify on: 2017/10/28
 
 #     ,,,         ,,,
 #   ;"   ';     ;'   ",
@@ -33,16 +33,16 @@ Hyper-parameters for training pipeline
                          after a dequeue, used to ensure a level
                          of mixing of elements
 
--2- Paraneters for Training:
+-2- Parameters for Training:
     - batch_size: int, the number of patches in one batch
     - num_epoches: int, the number of epoches
     - learning_rate_first: float, the learning rate for first epoch
     - learning_rate_last: float, the learning rate for last epoch
     - more parameters to be added
 
--3- Parameter for Constructing Model
-    - activation: string, indicate the activation method by either
-      "relu" or "lrelu" (leaky relu)
+-3- Parameters for Constructing Model
+    - activation: string, indicates the activation method by either
+      "relu" or "lrelu" (leaky relu) for general cnn models
     - alpha: float, slope of the leaky relu at x < 0
     - bn_momentum: float, momentum for removing average in batch
       normalization, typically values are 0.999, 0.99, 0.9, etc
@@ -56,6 +56,10 @@ import os
 import json
 from btc_settings import *
 
+
+'''
+Parameters for General CNN Models
+'''
 
 # Set path of the folder in where tfrecords are save in
 parent_dir = os.path.dirname(os.getcwd())
@@ -81,7 +85,7 @@ validate_num = 224
 # train_num = volumes_num["train"]
 # validate_num = volumes_num["validate"]
 
-parameters = {
+cnn_parameters = {
     # Basic settings
     "train_path": tpath,
     "validate_path": vpath,
@@ -96,9 +100,39 @@ parameters = {
     "num_epoches": 1,
     "learning_rate_first": 1e-3,
     "learning_rate_last": 1e-4,
+    "l2_loss_coeff": 0.001,
     # Parameter for model's structure
     "activation": "relu",  # "lrelu",
-    "alpha": 0.2,  # "lrelu"
-    "bn_momentum": 0.9,
+    "alpha": None,  # "lrelu"
+    "bn_momentum": 0.99,
+    "drop_rate": 0.5
+}
+
+
+'''
+Parameters for Autoencoder Models
+'''
+
+cae_parameters = {
+    # Basic settings
+    "train_path": tpath,
+    "validate_path": vpath,
+    "train_num": train_num,
+    "validate_num": validate_num,
+    "classes_num": 3,
+    "patch_shape": PATCH_SHAPE,
+    "capacity": 350,
+    "min_after_dequeue": 300,
+    # Parameters for training
+    "batch_size": 10,
+    "num_epoches": 1,
+    "learning_rate_first": 1e-3,
+    "learning_rate_last": 1e-4,
+    "l2_loss_coeff": 0.001,
+    "sparse_penalty_coeff": 0.001,
+    "sparse_level": 0.05,
+    # Parameter for model's structure
+    "activation": "relu",
+    "bn_momentum": 0.99,
     "drop_rate": 0.5
 }
