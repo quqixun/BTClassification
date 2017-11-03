3b697c56efbbe53e169f66c133b6af6470b914f0
Qixun Qu <quqixun@gmail.com>
2017-11-03 16:28:33 +0100

    Update test code for generating tfrecord files

--- src/test/test_tfrecords.py
+++ src/test/test_tfrecords.py
@@ -6,160 +6,160 @@ import tensorflow as tf
 from btc_settings import *
 
 
-# Path settings
-parent_dir = os.path.dirname(os.getcwd())
+# # Path settings
+# parent_dir = os.path.dirname(os.getcwd())
 
-input_dir = os.path.join(parent_dir, DATA_FOLDER, AUGMENT_FOLDER)
-label_file = os.path.join(parent_dir, DATA_FOLDER, LABEL_FILE)
+# input_dir = os.path.join(parent_dir, DATA_FOLDER, AUGMENT_FOLDER)
+# label_file = os.path.join(parent_dir, DATA_FOLDER, LABEL_FILE)
 
-# Load labels and cases' names
-labels = pd.read_csv(label_file)
-case_names = os.listdir(input_dir)
+# # Load labels and cases' names
+# labels = pd.read_csv(label_file)
+# case_names = os.listdir(input_dir)
 
-#
-# # Plot one case
-# import matplotlib.pyplot as plt
-#
-# idx = 7
-# case_no = case_names[idx]
-# case_grade = labels[GRADE_LABEL][labels[CASE_NO] == case_no].values[0]
+# #
+# # # Plot one case
+# # import matplotlib.pyplot as plt
+# #
+# # idx = 7
+# # case_no = case_names[idx]
+# # case_grade = labels[GRADE_LABEL][labels[CASE_NO] == case_no].values[0]
 
-# case_path = os.path.join(input_dir, case_no)
-# volume_names = os.listdir(case_path)
+# # case_path = os.path.join(input_dir, case_no)
+# # volume_names = os.listdir(case_path)
 
-# # Plot the middle slice of each volume
+# # # Plot the middle slice of each volume
 
-# slice_no = int((PARTIAL_SIZE + 1) / 2)
-# channel = 3
+# # slice_no = int((PARTIAL_SIZE + 1) / 2)
+# # channel = 3
 
-# volumes, vmins, vmaxs = [], [], []
-# for vn in volume_names:
-#     volume = np.load(os.path.join(case_path, vn))
-#     volumes.append(volume)
-#     vmins.append(np.min(volume[..., channel]))
-#     vmaxs.append(np.max(volume[..., channel]))
+# # volumes, vmins, vmaxs = [], [], []
+# # for vn in volume_names:
+# #     volume = np.load(os.path.join(case_path, vn))
+# #     volumes.append(volume)
+# #     vmins.append(np.min(volume[..., channel]))
+# #     vmaxs.append(np.max(volume[..., channel]))
 
-# vmin = np.min(vmins)
-# vmax = np.max(vmaxs)
+# # vmin = np.min(vmins)
+# # vmax = np.max(vmaxs)
 
-# column_num = 10
-# volumes_num = len(volumes)
-# row_num = np.ceil(volumes_num / column_num)
+# # column_num = 10
+# # volumes_num = len(volumes)
+# # row_num = np.ceil(volumes_num / column_num)
 
-# plt.figure()
-# for i in range(volumes_num):
-#     vc = volumes[i][..., channel]
-#     plt.subplot(row_num, column_num, i + 1)
-#     plt.axis("off")
-#     plt.imshow(vc[:, :, slice_no], cmap="gray", vmin=vmin, vmax=vmax)
-# mng = plt.get_current_fig_manager()
-# mng.resize(*mng.window.maxsize())
-# plt.show()
-
-
-# Obtain amount of each grade type
-grade2_num = 0
-grade3_num = 0
-grade4_num = 0
-
-for cn in case_names:
-    case_grade = labels[GRADE_LABEL][labels[CASE_NO] == cn].values[0]
-    volumes_num = len(os.listdir(os.path.join(input_dir, cn)))
-    if case_grade == GRADE_II:
-        grade2_num += volumes_num
-    elif case_grade == GRADE_III:
-        grade3_num += volumes_num
-    elif case_grade == GRADE_IV:
-        grade4_num += volumes_num
-    else:
-        # print("The grade of " + cn + " is unknown.")
-        continue
-
-print("Total number of patches: ", str(grade2_num + grade3_num + grade4_num))
-print("The number of Grade II patches: ", str(grade2_num))
-print("The number of Grade III patches: ", str(grade3_num))
-print("The number of Grade IV patches: ", str(grade4_num))
+# # plt.figure()
+# # for i in range(volumes_num):
+# #     vc = volumes[i][..., channel]
+# #     plt.subplot(row_num, column_num, i + 1)
+# #     plt.axis("off")
+# #     plt.imshow(vc[:, :, slice_no], cmap="gray", vmin=vmin, vmax=vmax)
+# # mng = plt.get_current_fig_manager()
+# # mng.resize(*mng.window.maxsize())
+# # plt.show()
 
 
-if os.path.isfile("train_cases.txt"):
-    os.remove("train_cases.txt")
-open("train_cases.txt", "a").close()
+# # Obtain amount of each grade type
+# grade2_num = 0
+# grade3_num = 0
+# grade4_num = 0
 
-if os.path.isfile("validate_cases.txt"):
-    os.remove("validate_cases.txt")
-open("validate_cases.txt", "a").close()
+# for cn in case_names:
+#     case_grade = labels[GRADE_LABEL][labels[CASE_NO] == cn].values[0]
+#     volumes_num = len(os.listdir(os.path.join(input_dir, cn)))
+#     if case_grade == GRADE_II:
+#         grade2_num += volumes_num
+#     elif case_grade == GRADE_III:
+#         grade3_num += volumes_num
+#     elif case_grade == GRADE_IV:
+#         grade4_num += volumes_num
+#     else:
+#         # print("The grade of " + cn + " is unknown.")
+#         continue
 
-
-def generate_cases_set(labels, grade):
-    # Set cases for training and validating
-    cases = labels[CASE_NO][labels[GRADE_LABEL] == grade].values.tolist()
-
-    # Generate cases' names for training set and validating set
-    cases_num = len(cases)
-    train_num = int(cases_num * PROPORTION)
-    index = list(np.arange(cases_num))
-    np.random.seed(RANDOM_SEED)
-    train_index = list(np.random.choice(cases_num, train_num, replace=False))
-    validate_index = [i for i in index if i not in train_index]
-    train = [[cases[i], grade] for i in train_index]
-    validate = [[cases[i], grade] for i in validate_index]
-
-    return train, validate
+# print("Total number of patches: ", str(grade2_num + grade3_num + grade4_num))
+# print("The number of Grade II patches: ", str(grade2_num))
+# print("The number of Grade III patches: ", str(grade3_num))
+# print("The number of Grade IV patches: ", str(grade4_num))
 
 
-# Write cases' names into text files
-def save_cases_names(file, cases):
-    txt = open(file, "a")
-    for case in cases:
-        txt.write("{}\n".format(case))
+# if os.path.isfile("train_cases.txt"):
+#     os.remove("train_cases.txt")
+# open("train_cases.txt", "a").close()
+
+# if os.path.isfile("validate_cases.txt"):
+#     os.remove("validate_cases.txt")
+# open("validate_cases.txt", "a").close()
 
 
-grade2_train, grade2_validate = generate_cases_set(labels, GRADE_II)
-# save_cases_names("train_cases.txt", grade2_train)
-# save_cases_names("validate_cases.txt", grade2_validate)
+# def generate_cases_set(labels, grade):
+#     # Set cases for training and validating
+#     cases = labels[CASE_NO][labels[GRADE_LABEL] == grade].values.tolist()
 
-grade3_train, grade3_validate = generate_cases_set(labels, GRADE_III)
-# save_cases_names("train_cases.txt", grade3_train)
-# save_cases_names("validate_cases.txt", grade3_validate)
+#     # Generate cases' names for training set and validating set
+#     cases_num = len(cases)
+#     train_num = int(cases_num * PROPORTION)
+#     index = list(np.arange(cases_num))
+#     np.random.seed(RANDOM_SEED)
+#     train_index = list(np.random.choice(cases_num, train_num, replace=False))
+#     validate_index = [i for i in index if i not in train_index]
+#     train = [[cases[i], grade] for i in train_index]
+#     validate = [[cases[i], grade] for i in validate_index]
 
-grade4_train, grade4_validate = generate_cases_set(labels, GRADE_IV)
-# save_cases_names("train_cases.txt", grade4_train)
-# save_cases_names("validate_cases.txt", grade4_validate)
-
-
-# Count patches in training set of each grade group
-
-
-def count_patches(input_dir, dataset):
-    num = 0
-    for d in dataset:
-        num += len(os.listdir(os.path.join(input_dir, d[0])))
-    return num
+#     return train, validate
 
 
-grade2_train_num = count_patches(input_dir, grade2_train)
-grade3_train_num = count_patches(input_dir, grade3_train)
-grade4_train_num = count_patches(input_dir, grade4_train)
-
-print("Total of training set: ", str(grade2_train_num + grade3_train_num + grade4_train_num))
-print("The number of Grade II patches: ", grade2_train_num)
-print("The number of Grade III patches: ", grade3_train_num)
-print("The number of Grade IV patches: ", grade4_train_num)
+# # Write cases' names into text files
+# def save_cases_names(file, cases):
+#     txt = open(file, "a")
+#     for case in cases:
+#         txt.write("{}\n".format(case))
 
 
-grade2_validate_num = count_patches(input_dir, grade2_validate)
-grade3_validate_num = count_patches(input_dir, grade3_validate)
-grade4_validate_num = count_patches(input_dir, grade4_validate)
+# grade2_train, grade2_validate = generate_cases_set(labels, GRADE_II)
+# # save_cases_names("train_cases.txt", grade2_train)
+# # save_cases_names("validate_cases.txt", grade2_validate)
 
-print("Total of validating set: ", str(grade2_validate_num + grade3_validate_num + grade4_validate_num))
-print("The number of Grade II patches: ", grade2_validate_num)
-print("The number of Grade III patches: ", grade3_validate_num)
-print("The number of Grade IV patches: ", grade4_validate_num)
+# grade3_train, grade3_validate = generate_cases_set(labels, GRADE_III)
+# # save_cases_names("train_cases.txt", grade3_train)
+# # save_cases_names("validate_cases.txt", grade3_validate)
 
-train_cases = grade2_train + grade3_train + grade4_train
-validate_cases = grade2_validate + grade3_validate + grade4_validate
-save_cases_names("train_cases.txt", train_cases)
-save_cases_names("validate_cases.txt", validate_cases)
+# grade4_train, grade4_validate = generate_cases_set(labels, GRADE_IV)
+# # save_cases_names("train_cases.txt", grade4_train)
+# # save_cases_names("validate_cases.txt", grade4_validate)
+
+
+# # Count patches in training set of each grade group
+
+
+# def count_patches(input_dir, dataset):
+#     num = 0
+#     for d in dataset:
+#         num += len(os.listdir(os.path.join(input_dir, d[0])))
+#     return num
+
+
+# grade2_train_num = count_patches(input_dir, grade2_train)
+# grade3_train_num = count_patches(input_dir, grade3_train)
+# grade4_train_num = count_patches(input_dir, grade4_train)
+
+# print("Total of training set: ", str(grade2_train_num + grade3_train_num + grade4_train_num))
+# print("The number of Grade II patches: ", grade2_train_num)
+# print("The number of Grade III patches: ", grade3_train_num)
+# print("The number of Grade IV patches: ", grade4_train_num)
+
+
+# grade2_validate_num = count_patches(input_dir, grade2_validate)
+# grade3_validate_num = count_patches(input_dir, grade3_validate)
+# grade4_validate_num = count_patches(input_dir, grade4_validate)
+
+# print("Total of validating set: ", str(grade2_validate_num + grade3_validate_num + grade4_validate_num))
+# print("The number of Grade II patches: ", grade2_validate_num)
+# print("The number of Grade III patches: ", grade3_validate_num)
+# print("The number of Grade IV patches: ", grade4_validate_num)
+
+# train_cases = grade2_train + grade3_train + grade4_train
+# validate_cases = grade2_validate + grade3_validate + grade4_validate
+# save_cases_names("train_cases.txt", train_cases)
+# save_cases_names("validate_cases.txt", validate_cases)
 
 # print(train_cases)
 # print(validate_cases)
@@ -184,13 +184,22 @@ save_cases_names("validate_cases.txt", validate_cases)
 # print(size49_num)
 
 
-partial_train_set = [["TCGA-CS-4942", 0], ["TCGA-CS-4944", 1], ["TCGA-02-0006", 2],
-                     ["TCGA-02-0009", 2], ["TCGA-02-0011", 2], ["TCGA-02-0027", 2]]
-partial_validate_set = [["TCGA-CS-6668", 0], ["TCGA-CS-5393", 1], ["TCGA-02-0033", 2],
-                        ["TCGA-02-0034", 2], ["TCGA-02-0037", 2], ["TCGA-02-0046", 2]]
+# partial_train_set = [["TCGA-CS-4944", 0], ["TCGA-CS-4942", 1], ["TCGA-02-0006", 2],
+#                      ["TCGA-02-0009", 2], ["TCGA-02-0011", 2], ["TCGA-02-0027", 2]]
+# partial_validate_set = [["TCGA-CS-6668", 0], ["TCGA-CS-5393", 1], ["TCGA-02-0033", 2],
+#                         ["TCGA-02-0034", 2], ["TCGA-02-0037", 2], ["TCGA-02-0046", 2]]
 
-partial_input_dir = os.path.join(parent_dir, DATA_FOLDER, "Augmented_Partial")
-output_dir = os.path.join(parent_dir, DATA_FOLDER, TFRECORDS_FOLDER)
+# partial_input_dir = os.path.join(parent_dir, DATA_FOLDER, "Augmented_Partial")
+# output_dir = os.path.join(parent_dir, DATA_FOLDER, TFRECORDS_FOLDER)
+# partial_train_set_path = os.path.join(output_dir, "partial_train.tfrecord")
+# partial_validate_set_path = os.path.join(output_dir, "partial_validate.tfrecord")
+
+partial_train_set = [["TCGA-CS-4944", 0], ["TCGA-CS-4942", 1], ["TCGA-02-0006", 2]]
+partial_validate_set = [["TCGA-CS-6668", 0], ["TCGA-CS-5393", 1], ["TCGA-02-0009", 2]]
+
+parent_dir = os.path.dirname(os.getcwd())
+partial_input_dir = os.path.join(parent_dir, DATA_FOLDER, "Volumes")
+output_dir = os.path.join(parent_dir, DATA_FOLDER, TFRECORDS_FOLDER, VOLUMES_FOLDER)
 partial_train_set_path = os.path.join(output_dir, "partial_train.tfrecord")
 partial_validate_set_path = os.path.join(output_dir, "partial_validate.tfrecord")
 
@@ -200,7 +209,15 @@ def create_tfrecord(input_dir, tfrecord_path, cases, mode):
     '''
 
     def normalize(volume):
-        return (volume - np.min(volume)) / np.std(volume)
+        temp = np.copy(volume)
+        for c in range(CHANNELS):
+            channel = volume[..., c]
+            channel_object = channel[np.where(channel > 0)]
+            # if c == 0:
+                # print(np.min(channel_object), np.max(channel_object), np.mean(channel_object), np.std(channel_object))
+            # temp[..., c] = (channel - np.mean(channel_object)) / np.std(channel_object)
+            temp[..., c] = channel / np.max(channel)
+        return temp
 
     print("Create TFRecord to " + mode)
 
@@ -213,6 +230,8 @@ def create_tfrecord(input_dir, tfrecord_path, cases, mode):
                 continue
             volume = np.load(vp)
             volume = normalize(volume)
+            volume = volume[..., 0]
+            volume = np.reshape(volume, [112, 112, 88, 1])
             volume_raw = volume.tobytes()
             example = tf.train.Example(features=tf.train.Features(feature={
                 "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[case[1]])),
