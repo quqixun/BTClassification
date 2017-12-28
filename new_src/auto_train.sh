d8eedbf8e6db19af15477ec27b22d1f789026884
qiqi <quqixun@gmail.com>
2017-12-28 21:02:46 +0100

    Add more test

--- new_src/auto_train.sh
+++ new_src/auto_train.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-python train.py --volume t1ce --model pyramid --opt adam
-python train.py --volume flair --model pyramid --opt adam
-python train.py --volume t1ce --model vggish --opt adam
-python train.py --volume flair --model vggish --opt adam
+
+python train.py --volume t1ce --model pyramid --opt adam --seed 7
+
+# python train.py --volume t1ce --model pyramid --opt adam --seed 17
+# python train.py --volume t1ce --model pyramid --opt adam --seed 27
+# python train.py --volume t1ce --model pyramid --opt adam --seed 37
+# python train.py --volume t1ce --model pyramid --opt adam --seed 47
+# python train.py --volume t1ce --model pyramid --opt adam --seed 57
+# python train.py --volume t1ce --model pyramid --opt adam --seed 67
+# python train.py --volume t1ce --model pyramid --opt adam --seed 77
+# python train.py --volume t1ce --model pyramid --opt adam --seed 87
+# python train.py --volume t1ce --model pyramid --opt adam --seed 97
+# python train.py --volume t1ce --model pyramid --opt adam --seed 107
+# python train.py --volume t1ce --model pyramid --opt adam --seed 117
+# python train.py --volume t1ce --model pyramid --opt adam --seed 127
+# python train.py --volume t1ce --model pyramid --opt adam --seed 137
+# python train.py --volume t1ce --model pyramid --opt adam --seed 147
+# python train.py --volume t1ce --model pyramid --opt adam --seed 157
+# python train.py --volume t1ce --model pyramid --opt adam --seed 167
+# python train.py --volume t1ce --model pyramid --opt adam --seed 177
+# python train.py --volume t1ce --model pyramid --opt adam --seed 187
+# python train.py --volume t1ce --model pyramid --opt adam --seed 197
+
+# python train.py --volume t1ce --model vggish --opt adam
+# python train.py --volume flair --model pyramid --opt adam
+# python train.py --volume flair --model vggish --opt adam
