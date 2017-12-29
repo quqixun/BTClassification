59321e429a7ca279a8fd1281b1d08054fb2b8820
qiqi <quqixun@gmail.com>
2017-12-29 22:11:46 +0100

    Add commands for prediction

--- new_src/auto_train.sh
+++ new_src/auto_train.sh
@@ -1,27 +1,75 @@
 #!/bin/sh
 
-python train.py --volume t1ce --model pyramid --opt adam --seed 7
+# train
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 7
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 17
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 27
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 37
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 47
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 57
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 67
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 77
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 87
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 97
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 107
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 117
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 127
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 137
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 147
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 157
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 167
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 177
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 187
+# python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 197
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 207
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 217
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 227
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 237
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 247
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 257
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 267
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 277
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 287
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 297
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 307
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 317
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 327
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 337
+python train.py --mode train --volume t1ce --model pyramid --opt adam --seed 347
 
-# python train.py --volume t1ce --model pyramid --opt adam --seed 17
-# python train.py --volume t1ce --model pyramid --opt adam --seed 27
-# python train.py --volume t1ce --model pyramid --opt adam --seed 37
-# python train.py --volume t1ce --model pyramid --opt adam --seed 47
-# python train.py --volume t1ce --model pyramid --opt adam --seed 57
-# python train.py --volume t1ce --model pyramid --opt adam --seed 67
-# python train.py --volume t1ce --model pyramid --opt adam --seed 77
-# python train.py --volume t1ce --model pyramid --opt adam --seed 87
-# python train.py --volume t1ce --model pyramid --opt adam --seed 97
-# python train.py --volume t1ce --model pyramid --opt adam --seed 107
-# python train.py --volume t1ce --model pyramid --opt adam --seed 117
-# python train.py --volume t1ce --model pyramid --opt adam --seed 127
-# python train.py --volume t1ce --model pyramid --opt adam --seed 137
-# python train.py --volume t1ce --model pyramid --opt adam --seed 147
-# python train.py --volume t1ce --model pyramid --opt adam --seed 157
-# python train.py --volume t1ce --model pyramid --opt adam --seed 167
-# python train.py --volume t1ce --model pyramid --opt adam --seed 177
-# python train.py --volume t1ce --model pyramid --opt adam --seed 187
-# python train.py --volume t1ce --model pyramid --opt adam --seed 197
-
-# python train.py --volume t1ce --model vggish --opt adam
-# python train.py --volume flair --model pyramid --opt adam
-# python train.py --volume flair --model vggish --opt adam
+# test
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 7
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 17
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 27
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 37
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 47
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 57
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 67
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 77
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 87
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 97
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 107
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 117
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 127
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 137
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 147
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 157
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 167
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 177
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 187
+# python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 197
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 207
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 217
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 227
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 237
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 247
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 257
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 267
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 277
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 287
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 297
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 307
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 317
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 327
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 337
+python train.py --mode test --volume t1ce --model pyramid --opt adam --seed 347
