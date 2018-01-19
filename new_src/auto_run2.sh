4103b74c88fb72dee7f66ca63a670aae45bb135f
qiqi <quqixun@gmail.com>
2018-01-19 20:29:22 +0100

    Bash command

---
+++ new_src/auto_run2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# python train.py --model model0
+# python train.py --model model1
+# python train.py --model model2
+# python train.py --model model3
+# python train.py --model model4
+python train.py --model model5
+python train.py --model model6
+python train.py --model model7
+python train.py --model model8
+python train.py --model model9
+python train.py --model model10
+python train.py --model model11
+python train.py --model model12
+python train.py --model model13
+python train.py --model model14
+python train.py --model model15
+python train.py --model model16
+python train.py --model model17
+python train.py --model model18
+python train.py --model model19
+python train.py --model model20
+python train.py --model model21
+python train.py --model model22
